2697a8aa72e470749d57262a6ceb569078aaedf0
Andrew Zaborowski <balrogg@gmail.com>
2019-12-22 18:52:05 +0000

    Rewrite based on the code of the nmap integration
    Devices are now tracked based on the MAC, not IP.  The tracker reports
    the IP, the MAC and the name (if available) from the ARP tables.  We use
    arp -a to list the devices currently visible, ping them once each, and
    report to HA if ping reply received.  Seems to work reliably with my
    Android phone.  The config options are now more like the nmap options
    (hosts and exclude) but I kept the interface option from the arp+ping
    component.  We ping all the devices found in the arp tables rather than
    needing a list of devices to track, except for devices outside of the
    "hosts" list or matching the "exclude" list.  The format now allows
    netmasks like 10.0.0.0/16, similar to nmap.

--- device_tracker.py
+++ device_tracker.py
@@ -1,110 +1,195 @@
-"""
-Tracks devices by sending a ICMP echo request (ping) and query arp table.
-
-For more details about this platform, please refer to the documentation at
-https://home-assistant.io/components/device_tracker.ping/
-"""
-
+"""Track devices from the ARP table using ping."""
+from collections import namedtuple
+from datetime import timedelta
 import logging
 import subprocess
-import sys
 import re
-from datetime import timedelta
 
 import voluptuous as vol
 
-import homeassistant.helpers.config_validation as cv
 from homeassistant.components.device_tracker import (
-    PLATFORM_SCHEMA)
-from homeassistant.components.device_tracker.const import (
-    CONF_SCAN_INTERVAL, SCAN_INTERVAL, SOURCE_TYPE_ROUTER)
-from homeassistant import util
-from homeassistant import const
+    DOMAIN,
+    PLATFORM_SCHEMA,
+    DeviceScanner,
+)
+from homeassistant.const import CONF_HOSTS
+import homeassistant.helpers.config_validation as cv
+import homeassistant.util.dt as dt_util
 
 _LOGGER = logging.getLogger(__name__)
 
-CONF_PING_COUNT = 'count'
-CONF_IFACE = 'iface'
+CONF_EXCLUDE = "exclude"
+# Interval in minutes to exclude devices from a scan while they are home
+CONF_HOME_INTERVAL = "home_interval"
+CONF_IFACE = "iface"
+CONF_PING_TIMEOUT = "ping_timeout"
 
-PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend({
-    vol.Required(const.CONF_HOSTS): {cv.string: cv.string},
-    vol.Optional(CONF_PING_COUNT, default=1): cv.positive_int,
-    vol.Optional(CONF_IFACE): cv.string,
-})
+PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend(
+    {
+        vol.Required(CONF_HOME_INTERVAL, default=0): cv.positive_int,
+        vol.Optional(CONF_HOSTS, default=[]): vol.All(cv.ensure_list, [cv.string]),
+        vol.Optional(CONF_EXCLUDE, default=[]): vol.All(cv.ensure_list, [cv.string]),
+        vol.Optional(CONF_PING_TIMEOUT, default=2.0): vol.All(vol.Coerce(float), vol.Range(min=0, max=5)),
+        vol.Optional(CONF_IFACE, default=''): cv.string,
+    }
+)
 
+def get_scanner(hass, config):
+    """Validate the configuration and return the scanner."""
+    return ArpDeviceScanner(config[DOMAIN])
 
-class Host:
-    """Host object with ping detection."""
+Device = namedtuple("Device", ["mac", "name", "ip", "last_update"])
 
-    def __init__(self, ip_address, dev_id, hass, config):
-        """Initialize the Host pinger."""
-        self.hass = hass
-        self.ip_address = ip_address
-        self.dev_id = dev_id
-        self._count = config[CONF_PING_COUNT]
-        self._iface = config[CONF_IFACE]
-
-        if self._iface:
-            self._ping_cmd = ['ping', '-I', self._iface, '-n', '-q', '-c1', '-W1', self.ip_address]
-            self._parp_cmd = ['arp', '-i', self._iface, '-n', self.ip_address]
-        else:
-            self._ping_cmd = ['ping', '-n', '-q', '-c1', '-W1', self.ip_address]
-            self._parp_cmd = ['arp', '-n', self.ip_address]
-
-    def ping(self):
-        """Send an ICMP echo request and return True if success."""
-        pinger = subprocess.Popen(self._ping_cmd, stdout=subprocess.PIPE, stderr=subprocess.DEVNULL)
-        try:
-            pinger.communicate()
-            return pinger.returncode == 0
-        except subprocess.CalledProcessError:
-            return False
-
-   
-    def parp(self):
-        """Get the MAC address for a given IP."""
-        arp = subprocess.Popen(self._parp_cmd, stdout=subprocess.PIPE)
-        out, _ = arp.communicate() 
-        match = re.search(r'(([0-9A-Fa-f]{1,2}\:){5}[0-9A-Fa-f]{1,2})', str(out))
-        if match:
-             return True
+def ip_mask_match(ip, mask):
+    # Supported formats:
+    # a.b.c.d/n
+    # A.B.C.D
+    # where a, b, c, d, n must be decimal numbers, and A, B, C, D
+    # may be either a decimal number or a range (x-y)
+    addr = ip.split('.', 3)
+    if not addr:
         return False
 
-    def update(self, see):
-        """Update device state by sending one or more ping messages."""
-        failed = 0
-        while failed < self._count:  # check more times if host is unreachable
+    abcdn = re.match(r'([0-9]+)\.([0-9]+)\.([0-9]+)\.([0-9]+)/([0-9]+)', mask)
+    if abcdn:
+        addr_num = (int(addr[0]) << 24) | (int(addr[1]) << 16) | (int(addr[2]) << 8) | (int(addr[3]) << 0)
+        mask_num = (int(abcdn[1]) << 24) | (int(abcdn[2]) << 16) | (int(abcdn[3]) << 8) | (int(abcdn[4]) << 0)
+        bits = int(abcdn[5])
+        if bits < 1 or bits > 32:
+            return False
+        return addr_num >> (32 - bits) == mask_num >> (32 - bits)
 
-            if self.ping():
-                see(dev_id=self.dev_id, source_type=SOURCE_TYPE_ROUTER)
-                _LOGGER.info("Ping OK from %s", self.ip_address)
-                return True
-            _LOGGER.info("No response from %s failed=%d", self.ip_address, failed)
-           
-            if self.parp():  
-                 see(dev_id=self.dev_id, source_type=SOURCE_TYPE_ROUTER)
-                 _LOGGER.info("Arp OK from %s", self.ip_address)
-                 return True
-            _LOGGER.info("No MAC address found") 
-            failed += 1 
+    for a, b in zip(addr, mask.split('.', 3)):
+        ip_num = int(a)
+        xy = re.match(r'([0-9]+)-([0-9]+)', b)
+        if xy:
+            if ip_num < int(xy[1]) or ip_num > int(xy[2]):
+                return False
+        else:
+            if ip_num != int(b):
+                return False
 
-def setup_scanner(hass, config, see, discovery_info=None):
-    """Set up the Host objects and return the update function."""
-    hosts = [Host(ip, dev_id, hass, config) for (dev_id, ip) in
-             config[const.CONF_HOSTS].items()]
-
-    interval = config.get(CONF_SCAN_INTERVAL, timedelta(seconds=len(hosts) * config[CONF_PING_COUNT]) + SCAN_INTERVAL)
-
-    _LOGGER.info("Started ping tracker with interval=%s on hosts: %s", interval, ",".join([host.ip_address for host in hosts]))
-
-    def update_interval(now):
-        """Update all the hosts on every interval time."""
-        try:
-            for host in hosts:
-                host.update(see)
-        finally:
-            hass.helpers.event.track_point_in_utc_time(
-                update_interval, util.dt.utcnow() + interval)
-
-    update_interval(None)
     return True
+
+class ArpDeviceScanner(DeviceScanner):
+    """This class scans for devices using arp+ping."""
+
+    exclude = []
+
+    def __init__(self, config):
+        """Initialize the scanner."""
+        self.last_results = []
+
+        self.hosts = config[CONF_HOSTS]
+        self.exclude = config[CONF_EXCLUDE]
+        minutes = config[CONF_HOME_INTERVAL]
+        self._ping_timeout = config[CONF_PING_TIMEOUT]
+        self._iface = config[CONF_IFACE]
+        self.home_interval = timedelta(minutes=minutes)
+
+        _LOGGER.debug("Scanner initialized")
+
+    def scan_devices(self):
+        """Scan for new devices and return a list with found device IDs."""
+        self._update_info()
+
+        _LOGGER.debug("last results %s", self.last_results)
+
+        return [device.mac for device in self.last_results]
+
+    def get_device_name(self, device):
+        """Return the name of the given device or None if we don't know."""
+        filter_named = [
+            result.name for result in self.last_results if result.mac == device
+        ]
+
+        if filter_named:
+            return filter_named[0]
+        return None
+
+    def get_extra_attributes(self, device):
+        """Return the IP of the given device."""
+        filter_ip = next(
+            (result.ip for result in self.last_results if result.mac == device), None
+        )
+        return {"ip": filter_ip}
+
+    def _update_info(self):
+        """Scan the network for devices.
+
+        Returns boolean if scanning successful.
+        """
+
+        last_results = []
+        last_macs = []
+        if self.home_interval:
+            boundary = dt_util.now() - self.home_interval
+            for device in self.last_results:
+                if device.last_update > boundary:
+                    last_results.append(device)
+                    last_macs.append(device.mac)
+
+        cmd = [ 'arp', '-a' ]
+        if self._iface:
+            cmd += [ '-i', self._iface ]
+        _LOGGER.debug('Running ' + ' '.join(cmd) + '...')
+        try:
+            arp = subprocess.Popen(cmd, stdout=subprocess.PIPE)
+            out, _ = arp.communicate() 
+        except Exception as e:
+            _LOGGER.error('Fail: ' + ' '.join(cmd) + ' resulted in ' + str(e))
+            return False
+
+        now = dt_util.now()
+        for line in out.decode('utf-8').splitlines():
+            # Format is: <hostname>|? (<ip>) at <mac> [<iftype>] on <iface>
+            # The hostname is optional for us.  If we can't parse the IP or the
+            # MAC the result is useless to us though so skip it.
+            r = r'([a-zA-Z0-9_.-]+|\?) \((([0-9]+\.){3}[0-9]+)\) at (([0-9A-Fa-f]{2}\:){5}[0-9A-Fa-f]{2}) .*'
+            m = re.match(r, line)
+            if not m:
+                continue
+
+            if m[1] == '?':
+                hostname = None
+            else:
+                hostname = m[1]
+            ipv4 = m[2]
+            mac = m[4]
+
+            if mac in last_macs:
+                continue
+
+            for mask in self.hosts:
+                if ip_mask_match(ipv4, mask):
+                    break
+            else:
+                continue
+
+            skip = False
+            for mask in self.exclude:
+                if ip_mask_match(ipv4, mask):
+                    skip = True
+                    break
+            if skip:
+                continue
+
+            cmd = [ 'ping', '-n', '-q', '-c1', '-W' + str(self._ping_timeout) ]
+            if self._iface:
+                cmd += [ '-I', self._iface ]
+            cmd += [ ipv4 ]
+            _LOGGER.debug('Running ' + ' '.join(cmd) + '...')
+            try:
+                pinger = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.DEVNULL)
+                pinger.communicate()
+                if pinger.returncode != 0:
+                    continue
+            except:
+                continue
+
+            last_results.append(Device(mac.upper(), hostname, ipv4, now))
+
+        self.last_results = last_results
+
+        _LOGGER.debug("arp scan done")
+        return True
